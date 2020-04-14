c3542d26f8cf02e8c56e92b797002dab11f65a62
Anjey Novicki <anjeynov@gmail.com>
2020-04-14 17:32:27 +0300

    feat: add collection view and images

--- Code/ARKit-Drawing/Options/BasicShape.swift
+++ Code/ARKit-Drawing/Options/BasicShape.swift
@@ -8,9 +8,9 @@ enum ShapeOption: String, RawRepresentable {
     case resetScene = "Reset Scene"
 }
 
-enum Shape: String {
-    case box = "Box", sphere = "Sphere", cylinder = "Cylinder", cone = "Cone", torus = "Torus"
-}
+//enum Shape: String {
+//    case box = "Box", sphere = "Sphere", cylinder = "Cylinder", cone = "Cone", torus = "Torus"
+//}
 
 enum Size: String {
     case small = "Small", medium = "Medium", large = "Large"
